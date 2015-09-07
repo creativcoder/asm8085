b1dd585d9cab56bfc3ae405b5e28d1e370b8e808
Rahul Sharma <rsconceptx@gmail.com>
2015-09-08 01:36:43 +0530

    updated sort.asm

--- sort.asm
+++ sort.asm
@@ -7,20 +7,21 @@ jmp start
 
 ;code
 start: lxi h,0000h
-outer: mvi b,09h
-inner: mvi c,09h
-work: mov a,m
+mvi b,04h
+outer:  mvi c,04h
+lxi h,0000h
+inner: mov a,m
 inx h
 cmp m
-jc skip       ; dont exchange if <
-jz skip       ;dont exchange if =
+jc skip ; dont exchange if <
+jz skip ;dont exchange if =
 mov d,m
 mov m,a
 dcx h
 mov m,d
 inx h
 skip: dcr c
-jnz work
+jnz inner
 dcr b
 jnz outer
 hlt
