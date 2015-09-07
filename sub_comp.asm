8b13d41126fa68f3286f4db7791eff5af313db5d
Rahul Sharma <rsconceptx@gmail.com>
2015-09-07 22:09:26 +0530

    added compliment subtraction

---
+++ sub_comp.asm
@@ -0,0 +1,27 @@
+;Rahul Sharma
+;Subtract two numbers using two's compliment
+
+jmp start
+
+;data
+
+
+;code
+start: lxi h,0000h
+mov a,m
+inx h
+mvi c,00h
+cmp m
+sub m
+jnc str
+inr c
+cma 
+inr a
+str: inx h
+mov m,a
+inx h
+mov m,c
+
+sub b
+
+hlt
