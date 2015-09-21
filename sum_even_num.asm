aafc2a98678b4a838006aa6df76e5273df704c29
Rahul Sharma <rsconceptx@gmail.com>
2015-09-22 00:38:01 +0530

    Update and rename sum_odd_num.asm to sum_even_num.asm

--- sum_even_num.asm
+++ sum_even_num.asm
@@ -7,20 +7,23 @@ jmp start
 
 
 ;code
-start: lxi h,0000h  ; 0000 contains the range of numbers
+start: lxi h,0000h
 mov c,m
-mvi e,00h ; buffer to store accumulating values
+mvi e,00h
+mvi b,00h
 inx h
 loop: mov a,m
 rar
-jnc skip
+jc skip
 mov a,m
 add e
-mov e,a
-mvi a,00h
+jnc incr
+inr b
+incr: mov e,a
 skip: dcr c
 inx h
 jnz loop
+mov m,b
+inx h
 mov m,e
-
 hlt
