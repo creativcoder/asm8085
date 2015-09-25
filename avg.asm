448d1d19bd78afded013d48fcfc5eea64a83ae14
Rahul Sharma <rsconceptx@gmail.com>
2015-09-25 11:52:43 +0530

    Create avg.asm

---
+++ avg.asm
@@ -0,0 +1,29 @@
+
+;Program to calculate avg of numbers
+
+jmp start
+
+;data
+
+
+;code
+start: lxi h,0000h
+mov c,m
+mov d,m
+mvi e,00h
+inx h
+loop: mov a,m
+adc e
+mov e,a
+dcr c
+inx h
+jnz loop
+mov c,d
+mvi b,00h
+mov a,e
+avg: inr b
+sub d
+jz ex
+jnz avg
+ex: mov m,b
+hlt
