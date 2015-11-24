739b310f0048c2be5e674921b0b03b0bb726f8c4
Rahul Sharma <rsconceptx@gmail.com>
2015-11-24 23:35:26 +0530

    Update sort.asm

--- sort.asm
+++ sort.asm
@@ -5,24 +5,25 @@ jmp start
 
 ;data
 
+
 ;code
-start: lxi h,0000h
-mvi b,04h
-outer: mov c,b
-lxi h, 0000h
-inner: mov a,m
+start: lxi h,0000h ;stores array length
+mov b,m
+dcr b
+oloop: mov c,b
+lxi h,0001h ; top most data of array
+iloop: mov a,m
 inx h
 cmp m
-jc skip
 jz skip
+jc skip
 mov e,m
 mov m,a
 dcx h
 mov m,e
 inx h
 skip: dcr c
-jnz inner
+jnz iloop
 dcr b
-jnz outer
-
+jnz oloop
 hlt
